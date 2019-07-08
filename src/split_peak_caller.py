7f14c12b005cf4b77666455ad66f5117a3a23cb8
Sager Gosai <sgosai@g.harvard.edu>
2019-07-08 05:55:04 +0000

    helper bash script

---
+++ src/split_peak_caller.py
@@ -0,0 +1,16 @@
+source activate hff
+
+cd /n/local/hcr-ff/analysis
+
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__0_10.bed -ji 0 -jr 10 -ws 100 -ss 100 -gs
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__1_10.bed -ji 1 -jr 10 -ws 100 -ss 100 -gs
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__2_10.bed -ji 2 -jr 10 -ws 100 -ss 100 -gs
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__3_10.bed -ji 3 -jr 10 -ws 100 -ss 100 -gs
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__4_10.bed -ji 4 -jr 10 -ws 100 -ss 100 -gs
+
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__5_10.bed -ji 5 -jr 10 -ws 100 -ss 100 -gs
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__6_10.bed -ji 6 -jr 10 -ws 100 -ss 100 -gs
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__7_10.bed -ji 7 -jr 10 -ws 100 -ss 100 -gs
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__8_10.bed -ji 8 -jr 10 -ws 100 -ss 100 -gs
+python /n/local/hcr-ff/hcr-ff/call_peaks.py /n/local/hcr-ff/data/FASTQ/FEN1_rep11detailed.txt FEN1_rep11__9_10.bed -ji 9 -jr 10 -ws 100 -ss 100 -gs
+
